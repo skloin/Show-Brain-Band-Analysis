522abfa69ee2eb3bf7e0fa78fa5dcf444a69dfd1
skloin <skloin@gmail.com>
2025-12-25 16:37:11 -0500

    Update title from 'SB Artist Affordability Calculator' to 'Show Brain Artist Analysis'

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("SB Artist Affordability Calculator")
+st.title("Show Brain Artist Analysis")
 
 # --- Configuration Sidebar ---
 st.sidebar.header("Budget Configuration")
